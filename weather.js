03ed11a52ff51c989a4200c31d360f6debbde932
Atun <mali1996ua@gmail.com>
2022-09-28 22:21:32 +0300

    try-catch

--- weather.js
+++ weather.js
@@ -27,36 +27,38 @@ const getWeatherAppDataFromApi = async () => {
   const lang = "tr";
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${tokenKey}&units=${units}&lang=${lang}`;
 
-  const response = await fetch(url).then((response) => response.json());
-  console.log(response);
+  try {
+    const response = await fetch(url).then((response) => response.json());
+    console.log(response);
 
-  const { main, sys, weather, name } = response;
+    const { main, sys, weather, name } = response;
 
-  const iconUrl = `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
-  const iconUrlAWS = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0].icon}.svg`;
+    const iconUrl = `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
+    const iconUrlAWS = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0].icon}.svg`;
 
-  const cityNameSpans = list.querySelectorAll(".city span");
-  const cityNameSpansArray = Array.from(cityNameSpans);
-  //! foreach array method aliyor.
-  if (cityNameSpansArray.length > 0) {
-    const filteredArray = cityNameSpansArray.filter(
-      (span) => span.innerText == name
-    );
-    if (filteredArray.length > 0) {
-      msg.innerText = `You already know the weather for ${name}, Please search for another city 🤔`;
-      //!timeout tek, interval tekrarli
-      setTimeout(() => {
-        msg.innerText = "";
-      }, 5000);
-      return;
+    const cityNameSpans = list.querySelectorAll(".city span");
+    const cityNameSpansArray = Array.from(cityNameSpans);
+    //! foreach array method aliyor.
+    if (cityNameSpansArray.length > 0) {
+      const filteredArray = cityNameSpansArray.filter(
+        (span) => span.innerText == name
+      );
+      if (filteredArray.length > 0) {
+        msg.innerText = `You already know the weather for ${name}, Please search for another city 🤔`;
+        //!timeout tek, interval tekrarli
+        setTimeout(() => {
+          msg.innerText = "";
+        }, 5000);
+        form.reset();
+        return;
+      }
     }
-  }
-  console.log(cityNameSpans);
-  const createdLi = document.createElement("li");
-  createdLi.classList.add("city");
-  createdLi.innerHTML = `<h2 class="city-name" data-name="${name}, ${
-    sys.country
-  }">
+    console.log(cityNameSpans);
+    const createdLi = document.createElement("li");
+    createdLi.classList.add("city");
+    createdLi.innerHTML = `<h2 class="city-name" data-name="${name}, ${
+      sys.country
+    }">
                 <span>${name}</span>
                 <sup>${sys.country}</sup>
             </h2>
@@ -65,7 +67,15 @@ const getWeatherAppDataFromApi = async () => {
                 <img class="city-icon" src="${iconUrl}">
                 <figcaption>${weather[0].description}</figcaption>
             </figure>`;
-  //*Append(son) vs. prepend(ilk)
-  list.append(createdLi);
+    //*Append(son) vs. prepend(ilk)
+    list.append(createdLi);
+    form.reset();
+  } catch (error) {
+    console.log(error);
+    msg.innerText = `404 (City not found)`;
+    setTimeout(() => {
+      msg.innerText = "";
+    }, 5000);
+  }
   form.reset();
 };
